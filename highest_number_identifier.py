fb92f091896529ec8657e11f05f293138fe4a228
OgmerKenshin <kenshinaogmer@gmail.com>
2024-10-14 01:30:34 +0800

    1st step

---
+++ highest_number_identifier.py
@@ -0,0 +1,11 @@
+#Since I can only use if statements then I can't use def
+
+num1 = float(input("please enter the 1st number: "))
+num2 = float(input("please enter the 2nd number: "))
+num3 = float(input("please enter the 3rd number: "))
+num4 = float(input("please enter the 4th number: "))
+num5 = float(input("please enter the 5th number: "))
+
+#I'm gonna assume the 1st number is the highest (not that it matters)
+
+highest_num = num1
